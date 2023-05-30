7df9b8c115e9a1967865b031ea6b5b89cd51b9da
Sovenyatko <alexandryshka@ukr.net>
2023-05-31 01:54:19 +0300

    Prak4

---
+++ Pr 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введiть рядок слiв: ");
+        string input = Console.ReadLine();
+        Console.Write("Введiть лiтеру: ");
+        char letter = Char.ToLower(Console.ReadKey().KeyChar);
+        
+        string[] words = input.Split(' ');
+       
+        Console.WriteLine("\nСлова, що починаються на лiтеру '{0}':", letter);
+
+        foreach (string word in words)
+        {
+            if (Char.ToLower(word[0]) == letter)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
